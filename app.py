3ac9f26fa988c146452c147e6d078b68d3812800
pagutaro-o0 <yuiopi.yuiopi.o00@gmail.com>
2026-01-31 03:13:11 +0900

    fix render port

--- app.py
+++ app.py
@@ -39,8 +39,7 @@ def go():
     return redirect(url_for("fortune", name=name))
 
 
-import os
-
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
